bcd48e647b6cf08831e7435c57d3396f83b26a03
arteybb <aphuwakorn2@gmail.com>
2025-05-09 15:19:21 +0700

    [update] register bcrypt

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,15 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from 'src/interface/user/user.interface';
 import { RegisterDTO } from './dto/user.dto';
 import { Model } from 'mongoose';
-
+import * as bcrypt from 'bcryptjs';
 @Injectable()
 export class UserService {
   constructor(@InjectModel('users') private readonly userModel: Model<User>) {}
 
   async register(registerDto: RegisterDTO): Promise<User> {
-    const user = new this.userModel(registerDto);
-    return user.save();
+    const user = await this.userModel.findOne({
+      username: registerDto.username,
+    });
+    if (user) {
+      throw new HttpException(
+        {
+          resCode: HttpStatus.CONFLICT,
+          resDesc: 'USER_ALREADY_EXISTS',
+        },
+        HttpStatus.CONFLICT,
+      );
+    }
+
+    const hashedPassword = await bcrypt.hash(registerDto.password, 10);
+
+    const newUser = new this.userModel({
+      ...registerDto,
+      password: hashedPassword,
+    });
+
+    return newUser.save();
   }
 }
